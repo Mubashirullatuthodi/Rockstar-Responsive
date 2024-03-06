8fdc345354d54b34eb592b601d22295b874fbde8
Mubashir Ullattuthodi <shanmubashir1@gmail.com>
2024-03-06 22:16:53 +0530

    added the project

---
+++ index.js
@@ -0,0 +1,20 @@
+ var title = document.getElementById("title-img");
+ var content = document.getElementById("title-content");
+
+function display1(){
+    title.innerHTML = "Grand Theft Auto VI";
+    content.innerHTML = "Trailer 1";
+}
+
+function display2(){
+    title.innerHTML = "Grand Theft Auto Online";
+    content.innerHTML = "Speed to victory in new drag Races";
+}
+function display3(){
+    title.innerHTML = "Grand Theft Auto Online";
+    content.innerHTML = "The Chop Shop";
+}
+function display4(){
+    title.innerHTML = "Grand Theft Auto Online";
+    content.innerHTML = "Celebrate Lunar New Year";
+}
